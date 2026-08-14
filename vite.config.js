a9c7d6dd677c5054f382f413921efe6a73cb8539
Luis A Sandoval <lasg.code@gmail.com>
2026-08-14 14:43:33 -0400

    Fix indentation in vite.config.js for better readability

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-    resolve: {
-      alias: {
-        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
+  base: '/',
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
 })
